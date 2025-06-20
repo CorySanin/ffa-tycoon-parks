3e8b83aa59783453cf77815d8cb266a458cee5c5
Cory Sanin <corysanin@outlook.com>
2025-06-20 00:13:47 -0500

    New metadata generation

--- plugin/src/metadata-read.ts
+++ plugin/src/metadata-read.ts
@@ -2,14 +2,16 @@
 
 (function () {
 
-    function writeMetadata(): void {
-        context.sharedStorage.set('meta.parkName', park.name);
-        context.sharedStorage.set('meta.description', scenario.details);
-        context.sharedStorage.set('meta.dimensions', map.size);
-        context.sharedStorage.set('meta.existingRides', map.rides);
+    function writeMetadata(unset: boolean = false): void {
+        const rides: string[] = (map.rides).filter(r => r.classification === 'ride' && r.status === 'open').map(r => r.name);
+        context.sharedStorage.set('meta.prettyParkName', unset ? null : park.name);
+        context.sharedStorage.set('meta.description', unset ? null : scenario.details);
+        context.sharedStorage.set('meta.dimensions', unset ? null : map.size);
+        context.sharedStorage.set('meta.existingRides', unset ? null : rides);
     }
 
     function main(): void {
+        writeMetadata(true);
         if (context.mode !== 'title') {
             writeMetadata();
         }
